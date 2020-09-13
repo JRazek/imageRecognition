3cf161ae5040c6a1f16e80e5531eb3fbd98838f6
Jakub <jakub.jakubecky123@gmail.com>
2020-09-13 22:32:07 +0200

    more structure2

--- src/main/java/jrazek/faces/recognition/structure/neural/convolutional/Kernel.java
+++ src/main/java/jrazek/faces/recognition/structure/neural/convolutional/Kernel.java
@@ -2,13 +2,16 @@ package jrazek.faces.recognition.structure.neural.convolutional;
 
 import jrazek.faces.recognition.structure.neural.Neuron;
 
+import javax.management.RuntimeErrorException;
 import java.util.LinkedList;
 
 public class Kernel extends Neuron {
-    int sizeX;
-    int sizeY;
-    LinkedList<Double> weights;
-    public Kernel(){
-
+    private int sizeX;
+    private int sizeY;
+    private LinkedList<Double> weights;
+    public Kernel(int x, int y) throws RuntimeErrorException {
+        if((x/2)*2 == x || (y/2)*2 == y)throw new RuntimeErrorException(new Error("the Kernel size must be odd number!"));
+        weights = new LinkedList<>();
     }
+
 }
