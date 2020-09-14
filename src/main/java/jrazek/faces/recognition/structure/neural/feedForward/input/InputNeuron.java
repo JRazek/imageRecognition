54764c0d88a8b0799a311cf48713dc7ec8447611
Jakub <jakub.jakubecky123@gmail.com>
2020-09-14 20:02:50 +0200

    s

--- src/main/java/jrazek/faces/recognition/structure/neural/feedForward/input/InputNeuron.java
+++ src/main/java/jrazek/faces/recognition/structure/neural/feedForward/input/InputNeuron.java
@@ -3,4 +3,5 @@ package jrazek.faces.recognition.structure.neural.feedForward.input;
 import jrazek.faces.recognition.structure.neural.feedForward.FFNeuron;
 
 public class InputNeuron extends FFNeuron {
+    //
 }
