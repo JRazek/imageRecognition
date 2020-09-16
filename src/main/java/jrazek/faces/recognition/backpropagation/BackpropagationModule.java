2006c7834c9b4d536675a37dea131b7d27e58ce8
Jakub <jakub.jakubecky123@gmail.com>
2020-09-16 22:59:23 +0200

    backprop file

---
+++ src/main/java/jrazek/faces/recognition/backpropagation/BackpropagationModule.java
@@ -0,0 +1,9 @@
+package jrazek.faces.recognition.backpropagation;
+
+import jrazek.faces.recognition.structure.Net;
+
+public class BackpropagationModule {
+    BackpropagationModule(Net net, double[] expected){
+
+    }
+}
